bcf8e45b29ebd524f0acfcdf91bcf8074e62563e
ychalodiya <ychalodiya@gmail.com>
2023-08-26 23:14:53 -0400

    initializing the project

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Amazon Marketplace</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
