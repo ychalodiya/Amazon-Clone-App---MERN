bc5115ee3f336099a1cadb0a082f04de1893096a
ychalodiya <ychalodiya@gmail.com>
2023-08-28 03:40:19 -0400

    implemented routing on SPA

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,24 @@
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './Components/Home';
+import Product from './Components/Product';
+
 function App() {
-  return (
-    <div>
-      <header>
-        <a href="/">Amazon Marketplace</a>
-      </header>
-      <main>list products</main>
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<div>
+				<header>
+					<Link to="/">Amazon Marketplace</Link>
+				</header>
+				<main>
+					<Routes>
+						<Route path="/product/:slug" element={<Product />} />
+						<Route path="/" element={<Home />} />
+					</Routes>
+				</main>
+			</div>
+		</BrowserRouter>
+	);
 }
 
 export default App;
